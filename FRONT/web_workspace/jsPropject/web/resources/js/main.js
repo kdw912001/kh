21d48a943a5e2128f578e42b604a84cbd813e4d2
kdw912001 <kdw912001@naver.com>
2019-02-12 21:27:14 +0900

    d

---
+++ FRONT/web_workspace/jsPropject/web/resources/js/main.js
@@ -0,0 +1,12 @@
+/** main.js
+ * 2019-02-12작성
+ * 자바스크립트 테스트용
+ */
+
+function testShow(){
+	window.alert("main.js 안에 있는 testShow() 실행됨");
+}
+
+function changeImage(){
+	document.getElementById("Image1").src = "/testj/resources/images/Android.gif";
+}
